c214ca93d0d027ec807cf4f0242fd3cc187e1421
moon gun <who307@naver.com>
2021-01-11 21:41:45 +0900

    수정

--- JS/toggle.js
+++ JS/toggle.js
@@ -7,8 +7,6 @@ const off = document.querySelector(".fa-toggle-off");
 const _body = document.querySelector("body")
 const _main = document.querySelector("main")
 
-
-
 function Dark() {
   on.classList.toggle("toggleOff");
   off.classList.toggle("toggleOn");
@@ -31,19 +29,41 @@ const conDiv1 = document.querySelector(".contents-div1");
 const conDiv2 = document.querySelector(".contents-div2");
 const conDiv3 = document.querySelector(".contents-div3");
 const _html = document.querySelector("html");
+
+
+
+let toggleEnd = false;
+
+document.addEventListener("scroll", function () {
+  let _scrollwidth = document.documentElement.scrollWidth;
+  console.log(_scrollwidth);
+  if(_scrollwidth > 1285){
+      return toggleEnd = true;
+  }
+  else{
+    return toggleEnd = false;
+    
+  };
+})
 function contentsClick1() {
-  conDiv1.classList.toggle("conClick");
+  if (toggleEnd  === true) {
+    conDiv1.classList.toggle("conClick");
+  }
   // _html.classList.toggle("scrollHidden");
 }
 function contentsClick2() {
-  conDiv2.classList.toggle("conClick");
-  // _html.classList.toggle("scrollHidden");
+  if (toggleEnd  === true) {
+    conDiv2.classList.toggle("conClick");
+    // _html.classList.toggle("scrollHidden");
+  }
 }
 function contentsClick3() {
-  conDiv3.classList.toggle("conClick");
+  if (toggleEnd  === true) {
+    conDiv3.classList.toggle("conClick");
+  }
   // _html.classList.toggle("scrollHidden");
 }
-
 conDiv1.addEventListener("click", contentsClick1);
 conDiv2.addEventListener("click", contentsClick2);
 conDiv3.addEventListener("click", contentsClick3);
+
